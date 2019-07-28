2ab8fcca3d5b92f21d5260eded5d863119292323
Leanne Cheung <leannecheung00@gmail.com>
2019-07-28 10:36:34 -0600

    ch5-multi-input-single-output

---
+++ ch5-multi-input-single-output.py
@@ -0,0 +1,50 @@
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+inputs = [toes[0], wlrec[0], nfans[0]]
+weights = [0,0,0]
+delta = [0,0,0]
+weight_deltas = [0,0,0]
+
+win_or_lose_binary = [1,1,0,1]
+true = win_or_lose_binary[0]
+
+alpha = 0.01
+
+def neural_network(inputs, weights):
+  prediction = 0
+  for i in range(3):
+    prediction += inputs[i] * weights[i]
+
+  return prediction
+
+def ele_mul(delta, inputs):
+  # result_weight_delta = 0
+
+  for i in range(3):
+    weight_deltas[i] = inputs[i] * delta * alpha
+    # result_weight_delta -= weight_deltas[i]
+
+  return weight_deltas
+
+
+
+
+
+for i in range(3):
+  prediction = neural_network(inputs, weights)
+  error = (prediction - true) ** 2
+  delta = prediction - true
+  weight_deltas = ele_mul(delta, inputs)
+
+  print('Error: ' + str(error) + " Prediction: " + str(prediction))
+  print('delta: ' + str(delta) + " weight_deltas: " + str(weight_deltas))
+  print('-------------')
+
+  for j in range(3):
+    weights[j] -= weight_deltas[j]
+
+print('Error: ' + str(error) + " Prediction: " + str(prediction))
+print('delta: ' + str(delta) + " weight_deltas: " + str(weight_deltas))
+
--- hot-cold-learning.py
+++ hot-cold-learning.py
@@ -1,12 +1,21 @@
 weight = 0.0
 goal_pred = 0.8
-input = 3
-alpha = 0.1
+input = 10
+
+
+
+alpha = 0.001
 
 for iteration in range(20):
   error = (input * weight - goal_pred) ** 2
   derivative = (input * weight - goal_pred) * input
-  weight = weight - derivative * alpha
+
   print('Iteration:' + str(iteration))
-  print("Error:" + str(error) + " Derivative:" + str(derivative))
+  print("Error:" + str(error) + " Weight:" + str(weight)  + " Derivative:" + str(derivative))
+
+  test = input * weight
+  print("input * weight: " + str(test))
   print('-------------')
+
+  weight = (weight - derivative) * alpha
+  
